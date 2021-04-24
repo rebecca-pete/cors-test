099a7b30e634b09d19f87cb90693b3ff97aa7697
Rebeccca Peterson <rebecca.peterson.soprano@gmail.com>
2021-04-23 18:47:28 -0700

    Update index.py

--- index.py
+++ index.py
@@ -5,11 +5,11 @@ class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.write(f"Served successfully.")
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", basicRequestHandler)
     ])
     port = 8888
     app.listen(port)
-    print(f"Application is ready and listening on port {port}")
+    print(f"Application is ready and listening on port {port}.")
     tornado.ioloop.IOLoop.current().start()
