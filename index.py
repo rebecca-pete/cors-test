789c44b17c048b9b05c909bf17458d33f29a2530
Rebeccca Peterson <rebecca.peterson.soprano@gmail.com>
2021-04-23 19:13:51 -0700

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,12 @@ import tornado.ioloop
 
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
         self.write(f"Served successfully.")
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
 
 if __name__ == "__main__":
     app = tornado.web.Application([
